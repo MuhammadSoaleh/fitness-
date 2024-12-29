ed72cbf35e3e0344478cd55771e22ffd15ef5b7f
unknown <m.soaleh63@gmail.com>
2024-12-29 16:06:21 +0500

    changes in nav bar

--- my-app/src/Nav.js
+++ my-app/src/Nav.js
@@ -3,45 +3,43 @@ import React from 'react'
 function Nav() {
   return (
     <div>
-      <nav className="navbar bg-dark border-bottom border-body d-flex" data-bs-theme="dark">
-      <div className="container-fluid">
+      <nav className="navbar bg-dark border-bottom border-body navbar navbar-expand-lg" data-bs-theme="dark">
+  <div className="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
         <li className="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a class="nav-link" href="/login">Sign In</a>
+          <a class="nav-link" href="#">Features</a>
         </li>
         <li className="nav-item">
-          <a class="nav-link" href="/register">Sign Up</a>
-        </li> 
-        {/* <li className="nav-item dropdown">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Dropdown link
           </a>
           <ul className="dropdown-menu">
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
-        </li> */}
-        
+        </li>
+        <div class="container-fluid">
+    <form className="d-flex" role="search">
+      <input className="form-control me-2" type="search" placeHolder="Search" aria-label="Search"/>
+      <button className="btn btn-secondary" type="submit">Search</button>
+    </form>
+  </div>
       </ul>
-      <form className="d-flex" role="search">
-        <input class="form-control w-25" type="search" placeHolder="Search" aria-label="Search"/>
-        <button class="btn btn-secondary mx-2" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
-
-
     </div>
   )
 }
